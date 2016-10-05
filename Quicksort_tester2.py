4d783fa6d596c2c1e6fbae2fa0cb5be0b2cb29f8
Jacqueline Watts <jacqui@Jacquelines-MBP.lan>
2016-10-05 00:49:53 -0700

    Recursion loop now working for half the recursive calls, only on split array of 0th to pivot element (not inclusive). Not yet executing properly for recursive calls on the pivot +1 element to the end of the array.

--- Quicksort_tester2.py
+++ Quicksort_tester2.py
@@ -1,5 +1,7 @@
 from random import choice
 
+# --------------------------------- DEFINING FUNCTIONS -------------------------
+
 def read_file_to_be_sorted(filename):
     """Load file and save as list to iterate over numbers."""
 	
@@ -15,9 +17,13 @@ def read_file_to_be_sorted(filename):
     return int_array
 
 
-def select_random_pivot(int_array):
+def select_random_pivot(given_array):
     """Define the pivot element using random module."""
-    return choice(int_array)
+
+    pivot = choice(given_array)
+    pivot_index = given_array.index(pivot)
+    return pivot, pivot_index
+
 
 def swap(item1, item2):
     """Swaps the placement of two elements in a list. """
@@ -28,49 +34,62 @@ def swap(item1, item2):
     return item1, item2
 
 
-def recursion_loop(int_array):
+def run_recursion(count, given_array, a, b = None):
     """Execute a single recursion loop around the randomly selected pivot element. """
-    
-    pivot = select_random_pivot(int_array)
-    pivot_index = int_array.index(pivot)
-    print "" 
-    print "The pivot element is {}.".format(pivot)
-    print ""
-    int_array[0], int_array[pivot_index] = swap(int_array[0], pivot)
 
-    i = 1
-    j = 1
-	
-    if len(int_array) >= 2:         
-        for int in int_array[1::]:
+    if len(given_array[a:b]) > 2:
+        # Grabs a random pivot and its index
+        pivot, pivot_index = select_random_pivot(given_array[a:b])
+        print "\nThis is recursion #{}:".format(count)
+        print "The pivot element is {}.".format(pivot)
+        
+        # Swaps the selected pivot with the 0th element in the array
+        given_array[a], given_array[pivot_index] = swap(given_array[a], pivot)
+
+        # Initiates i and j to the first non-pivot element
+        i = 1
+        j = 1
+        
+        # Loops through all non-pivot elements in array to compare > or < pivot element
+        for int in given_array[a+1:b]:
             if  int < pivot:
-                int_array[i], int_array[j] = swap(int_array[i], int)
+                given_array[i], given_array[j] = swap(given_array[i], int)
                 i += 1
             j += 1
 
-        int_array = swap_pivot(int_array, i, pivot)
-        return int_array
+        # When elements sorted around pivot, swaps pivot to absolute place
+        given_array[a], given_array[pivot-1] = swap(given_array[a], given_array[pivot-1])
 
-#         recursion_loop(int_array[0:(i-2)])
-#         recursion_loop(int_array[i::])
-#     
-#     else:
-#         return int_array
+        # Replaces int_array elements will corresponding given_array elements
+        int_array[a:pivot-1] = given_array[a:pivot-1]
+        int_array[pivot-1] = pivot
+        int_array[pivot:b] = given_array[pivot:b]
 
-def swap_pivot(int_array, i, pivot):
-    """Swap pivot element with (i-1)th element to put pivot in its absolute place. """
+        # FOR TROUBLESHOOTING: Prints the breakdown of one recursive loop
+        print "\nBreakdown after recursion #{}:".format(count)
+        print int_array[a:pivot-1]
+        print ""
+        print int_array[pivot-1]
+        print ""
+        print int_array[pivot:b]
+        count += 1
+        
+        # Calls a new recursion on the two new slices of array divided by our pivot
+        run_recursion(count, given_array, a, pivot-1)
+#         run_recursion(count, given_array[pivot:b], pivot, b)
     
-    int_array[0] = int_array[i-1]
-    int_array[i-1] = pivot
-    return int_array 
+    else:
+        # Checks for base case of two items and runs basic swap if needed
+        if (len(given_array) == 2) and (given_array[0] > given_array[1]):
+             given_array[0], given_array[1] = swap(given_array[0], given_array[1])
 
-# --------------------------------- CALLING FUNCTIONS --------------------------
+    return int_array
 
-int_array = read_file_to_be_sorted('QuickSort.txt')    
-int_array = recursion_loop(int_array)
+# --------------------------------- EXECUTABLE CODE --------------------------
+
+int_array = read_file_to_be_sorted('QuickSort_mini.txt')
+
+count = 1
+int_array = run_recursion(count, int_array, 0)
 print int_array
-
-
-# while len(int_array) > 1:
-# 	recursion_loop(int_array)
 
